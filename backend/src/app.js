fc58a915117ac83336c45e9d10b3cc2d5db5a85b
Icaro <icaro@Icaros-iMac.local>
2020-09-11 23:39:43 +1200

    Create our first route in express

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,10 @@ import express from 'express';
 const PORT = 3001;
 const app = express();
 
+app.get('/test', (request, response) => {
+  response.send('Hello from express!');
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
